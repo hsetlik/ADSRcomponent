b40f7e3acd76fe1c99672b61cff917d7c5a63e18
Hayden Setlik <haydensetlik@gmail.com>
2020-08-30 16:19:31 -0600

    added points, set up component listeners

--- Source/DraggersOnly.h
+++ Source/DraggersOnly.h
@@ -13,26 +13,28 @@ class ADSRenv : public juce::Component
 {
 public:
     ADSRenv() : attackDragger(30, 30, 80, 20, 20),
-                decaySlider(30, 30, 135, 100, 20),
-                releaseSlider(165, 30, 135, 100, 20)
+                decayDragger(30, 30, 135, 100, 20),
+                releaseDragger(165, 30, 135, 100, 20)
                 
     {
         //put the two sliders on and change their color to tell them apart
         addAndMakeVisible(attackDragger);
-        addAndMakeVisible(decaySlider);
-        addAndMakeVisible(releaseSlider);
+        addAndMakeVisible(decayDragger);
+        addAndMakeVisible(releaseDragger);
         
         setBounds(0, 0, 400, 300);
-        decaySlider.resetPointHome(25, 50);
-        releaseSlider.resetPointHome(25, 50);
+        decayDragger.resetPointHome(25, 50);
+        releaseDragger.resetPointHome(25, 50);
         
-        decaySlider.setChildColor(juce::Colours::orange);
-        releaseSlider.setChildColor(juce::Colours::green);
-        decaySlider.addPeer(&attackDragger, left);
-        decaySlider.addPeer(&releaseSlider, top);
-        releaseSlider.addPeer(&decaySlider, top);
+        decayDragger.setChildColor(juce::Colours::orange);
+        releaseDragger.setChildColor(juce::Colours::green);
+        decayDragger.addPeer(&attackDragger, left);
+        decayDragger.addPeer(&releaseDragger, top);
+        releaseDragger.addPeer(&decayDragger, top);
         
-        
+        pAttack = &attackDragger;
+        pDecay = &decayDragger;
+        pRelease = &releaseDragger;
     }
     ~ADSRenv()
     {
@@ -47,9 +49,11 @@ public:
     {
         //decayDragger.checkLimitUpdates();
     }
+    DraggerContainer* pAttack;
+    DraggerContainer* pDecay;
+    DraggerContainer* pRelease;
 private:
     DraggerContainer attackDragger;
-    DraggerContainer decaySlider;
-    DraggerContainer releaseSlider;
-
+    DraggerContainer decayDragger;
+    DraggerContainer releaseDragger;
 };
--- Source/GraphicADSR.h
+++ Source/GraphicADSR.h
@@ -9,17 +9,41 @@
 
 #include "DraggersOnly.h"
 
-class GraphicADSR : public juce::Component
+class GraphicADSR : public juce::Component,
+                    public juce::ComponentListener
 {
 public:
     GraphicADSR()
     {
         addAndMakeVisible(dragControllers);
+        dragControllers.pAttack->point.addComponentListener(this);
+        dragControllers.pDecay->point.addComponentListener(this);
+        dragControllers.pRelease->point.addComponentListener(this);
         
     }
     ~GraphicADSR() {}
+    void componentMovedOrResized(juce::Component& component, bool wasMoved, bool wasResized) override {calculatePoints();}
+    void calculatePoints()
+    {
+        
+    }
+    void paint(juce::Graphics &g) override
+    {
+        
+    }
 private:
     ADSRenv dragControllers;
+    //points for the lines to be drawn to
+    juce::Point<int> startPoint;
+    juce::Point<int> peakPoint;
+    juce::Point<int> sustainPoint;
+    juce::Point<int> releasePoint;
+    juce::Point<int> endPoint;
+    //values to pass to the Maximilian envelope
+    float attackMs;
+    float decayMs;
+    float sustainPercent;
+    float releaseMs;
 };
 
 
